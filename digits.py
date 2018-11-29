b15adc13854325c5341bf50259d8514d4a178bb6
v3tr0v <nikitavetrov@ya.ru>
2018-11-29 14:50:49 +0300

    Добавлены методы для чисел, их использование в основном скрипте

--- digits.py
+++ digits.py
@@ -1,11 +1,17 @@
 import random
 from typefunctions import basefuncs
+from typefunctions import digitfuncs
 
 i = random.randint(1, 10000)            # Случайное целое число из диапазона (x, y)
 f = random.uniform(1.0, 10000.0)        # Случайное число с плавающей точкой из диапазона (x, y)
+by = digitfuncs.to_bytes(random.randint(1, 10000))  # Представление рандомного целого числа в байтах
 
-print("Методы int:\n", basefuncs.methods_list(i), "\n")
-print("Методы float:\n", basefuncs.methods_list(f), "\n")
+# print("Методы int:\n", basefuncs.methods_list(i), "\n")
+# print("Методы float:\n", basefuncs.methods_list(f), "\n")
 
-print("Docstring int:\n", basefuncs.docstring(i), "\n")
-print("Docstring float:\n", basefuncs.docstring(f), "\n")
+# print("Docstring int:\n", basefuncs.docstring(i), "\n")
+# print("Docstring float:\n", basefuncs.docstring(f), "\n")
+
+print("Длина в битах числа %s:" % i, digitfuncs.bit_legth(i))
+print("Число %s в битах это:" % i, digitfuncs.to_bytes(i))
+print("Байты %s -- это число %s" % (by, digitfuncs.from_bytes(by)))
--- typefunctions/digitfuncs.py
+++ typefunctions/digitfuncs.py
@@ -1,1 +1,49 @@
-import random
+import sys
+
+
+def bit_legth(digit=1):
+    """
+    Количество бит передаваемого целого числа        
+    
+    Keyword Arguments:
+        digit {int} -- целое число на вход (default: {1})
+    
+    Returns:
+        digit.bit_length() -- Количество бит переданного целого числа
+    """
+    try:
+        return digit.bit_length()
+    except AttributeError:
+        raise
+
+def to_bytes(digit=1):
+    """Преобразование целого числа в bytes
+    
+    Keyword Arguments:
+        digit {int} -- целое число на вход (default: {1})
+    
+    Returns:
+        digit.to_bytes() -- представление в битах
+    """
+    try:
+        return digit.to_bytes(4, byteorder=sys.byteorder)
+    except (AttributeError, OverflowError):
+        raise
+
+def from_bytes(digit=1):
+    """
+    Преобразование в целое число из байтового представления digitfuncs.to_bytes()    
+    Keyword Arguments:
+        digit {int} -- целое число на вход (default: {1})
+    
+    Raises:
+        AttributeError -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+
+    try:
+        return int.from_bytes(digit, byteorder=sys.byteorder)
+    except AttributeError:
+        raise
