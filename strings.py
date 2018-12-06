8399aabdd5208a8021b1d025eeb993e66631d817
v3tr0v <nikitavetrov@ya.ru>
2018-12-06 22:20:27 +0300

    Добавлен коммент с методами типов

--- strings.py
+++ strings.py
@@ -6,6 +6,12 @@ print(l)
 
 """
 Методы str:
+'__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__',
+'__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__',
+'__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__',
+'__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
+'__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__'
+
 'capitalize', 'casefold', 'center', 'count','encode', 'endswith', 'expandtabs',
 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isdecimal', 'isdigit',
 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper',
@@ -13,4 +19,3 @@ print(l)
 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith',
 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
 """
-
